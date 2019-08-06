6321a4f79fe6d9668811b8e3686ff86b96734278
Ely De La Cruz <elycruz@elycruz.com>
2019-08-06 14:20:04 -0400

    issue-4 - Progress on input filter intiall implementation.

--- InputFilter.go
+++ InputFilter.go
@@ -1,15 +1,40 @@
 package ecms_go_inputfilter
 
 type InputFilter struct {
-	Inputs map[string]*Input
+	Inputs         map[string]*Input
 	BreakOnFailure bool
 }
 
 type InputFilterResult struct {
-	Result bool
-	Messages map[string][]string
-	validInputs map[string]*Input
-	invalidInputs map[string]*Input
-	validInputsAsList []*Input
+	Result              bool
+	Messages            map[string][]string
+	validInputs         map[string]*Input
+	invalidInputs       map[string]*Input
+	validInputsAsList   []*Input
 	invalidInputsAsList []*Input
+}
+
+type InputFilterInterface interface {
+	Validate(data map[string]interface{})
+	AddInput(i *Input)
+	AddInputs(i []*Input)
+}
+
+func (inputF *InputFilter) Validate(d map[string]interface{}) InputFilterResult {
+	ir := InputFilterResult{}
+	// @todo write body here
+	return ir
+}
+
+func (inputF *InputFilter) AddInput(i *Input) {
+	if i == nil {
+		return
+	}
+	inputF.Inputs[i.Name] = i
+}
+
+func (inputF *InputFilter) AddInputs(inputs []*Input) {
+	for _, i := range inputs {
+		inputF.AddInput(i)
+	}
 }
